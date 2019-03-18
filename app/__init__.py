5236cece2b3dc6d2ee95cabb89476d7c9e182450
Christopher-Polloni <47011990+Christopher-Polloni@users.noreply.github.com>
2019-03-17 21:43:19 -0400

    invites/add subgroup

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from flask_login import LoginManager
+from flask_moment import Moment
 
 db = SQLAlchemy()
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
+login = LoginManager(app)
+login = LoginManager(app)
+login.login_view = 'login'
+moment = Moment(app)
 
 from app import routes, models
